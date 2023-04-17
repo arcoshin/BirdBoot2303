890622cd7d4fef3672f1ab209ee4cdb5565a7b1f
arcoshin <arcoshin@gmail.com>
2023-04-17 21:26:21 +0800

    SpringBoot2303

---
+++ src/main/java/com/tedu/springboot2303/SpringBoot2303Application.java
@@ -0,0 +1,13 @@
+package com.tedu.springboot2303;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot2303Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot2303Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/tedu/springboot2303/entity/Article.java
@@ -0,0 +1,54 @@
+package com.tedu.springboot2303.entity;
+
+import java.io.Serializable;
+
+/**
+ * 文章模板類
+ */
+public class Article implements Serializable {
+    private String title;
+    private String author;
+    private String articleText;
+
+    public Article() {
+
+    }
+
+    public Article(String title, String author, String articleText) {
+        this.title = title;
+        this.author = author;
+        this.articleText = articleText;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getArticleText() {
+        return articleText;
+    }
+
+    public void setArticleText(String articleText) {
+        this.articleText = articleText;
+    }
+
+    @Override
+    public String toString() {
+        return "Article{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                '}';
+    }
+}
